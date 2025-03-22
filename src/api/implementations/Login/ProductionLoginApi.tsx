5c97c0dad8e11a79f0d5f382cb2eae9c3a46533f
umanari145 <umanari145@gmail.com>
2025-03-22 20:25:49 +0900

    本番データのレスポンスを修正

--- src/api/implementations/Login/ProductionLoginApi.tsx
+++ src/api/implementations/Login/ProductionLoginApi.tsx
@@ -4,19 +4,28 @@ import { LoginApiInterface } from "../../interface/LoginApiInterface";
 export class ProductionApi implements LoginApiInterface {
   async fetchData(email: string, password: string): Promise<{ success: boolean; httpStatusCode: number; message: string }> {
     try {
-      const response = await axios.post(
+      const {data}= await axios.post(
         `${process.env.REACT_APP_API_ENDPOINT}/api/login`,
         {
           email,
           password
         }
       );
-      // 成功時のレスポンス
-      return {
-        success: true,
-        httpStatusCode: response.status,
-        message: "ログイン成功",
-      };
+
+      if (data.statusCode === 200) {
+        // 成功時のレスポンス
+        return {
+          success: true,
+          httpStatusCode: data.statusCode,
+          message: "ログイン成功",
+        };
+      } else {
+        return {
+          success: false,
+          httpStatusCode: data.statusCode,
+          message: "ログイン失敗",
+        };
+      }
     } catch (error) {
       // エラーハンドリング
       if (axios.isAxiosError(error)  && error.response) {
